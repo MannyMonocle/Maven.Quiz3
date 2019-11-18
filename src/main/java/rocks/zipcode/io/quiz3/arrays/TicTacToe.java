eaa7e60ee250578c97ba301e1b48db88bc00e527
MannyMonocle <eetumadu@gmail.com>
2019-11-18 14:18:23 -0500

    tic tac toe

--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -1,37 +1,91 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class TicTacToe {
+    String[][] board;
 
     public TicTacToe(String[][] board) {
+        this.board = board;
     }
 
     public TicTacToe() {
+        board = new String[3][3];
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        return board[value];
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+        String[] ans = new String[3];
+        for(int i = 0; i < 3; i++){
+            ans[i] = board[i][value];
+        }
+        return ans;
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
-        return null;
+        Boolean ans = false;
+        String[] row = getRow(rowIndex);
+        if(row[0] == row[1] && row[1] == row[2]){ans = true;}
+        return ans;
     }
 
     public Boolean isColumnHomogeneous(Integer columnIndex) {
-        return null;
+        Boolean ans = false;
+        String[] column = getColumn(columnIndex);
+        if(column[0] == column[1] && column[1] == column[2]){ans = true;}
+        return ans;
+    }
+
+    public Boolean isDiagnol1Won(){
+        Boolean ans = false;
+        if(board[0][0] == board[1][1] && board[1][1] == board[2][2]){ans = true;}
+        return ans;
+    }
+
+    public Boolean isDiagnol2Won(){
+        Boolean ans = false;
+        if(board[0][2] == board[1][1] && board[1][1] == board[2][0]){ans = true;}
+        return ans;
     }
 
     public String getWinner() {
-        return null;
+        String ans = rowWinner() + columnWinner() + diagnolWinner();
+        return Character.toString(ans.charAt(0));
     }
 
     public String[][] getBoard() {
-        return null;
+        return board;
     }
+
+
+    public String rowWinner(){
+        for(int i = 0; i < 3; i++){
+            if(isRowHomogenous(i)){
+                return getRow(i)[0];
+            }
+        }
+        return "";
+    }
+
+    public String columnWinner(){
+        for(int i = 0; i < 3; i++){
+            if(isColumnHomogeneous(i)){
+                return getColumn(i)[0];
+            }
+        }
+        return "";
+    }
+
+    public String diagnolWinner(){
+        if(isDiagnol1Won()){return board[0][0];}
+        else if(isDiagnol2Won()){return board[2][0];}
+        else {return "";}
+    }
+
 }
