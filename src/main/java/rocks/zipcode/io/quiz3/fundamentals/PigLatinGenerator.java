19ecee8eeb15c38b74027b4283f79c471fb73aa6
MannyMonocle <eetumadu@gmail.com>
2019-11-18 16:33:14 -0500

    translate sentence

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -4,7 +4,41 @@ package rocks.zipcode.io.quiz3.fundamentals;
  * @author leon on 09/12/2018.
  */
 public class PigLatinGenerator {
+
+
     public String translate(String str) {
-        return null;
+        String[] splitArray = words(str);
+        //String newString = "";
+
+        for(int i = 0; i < splitArray.length; i++){
+            String word = splitArray[i];
+            word = reshuffle(word);
+            word = addEnd(word);
+            splitArray[i] = word;
+        }
+
+        return String.join(" ", splitArray);
     }
+
+    private String[] words(String str){
+        return str.split(" ");
+    }
+
+    private String addEnd(String str){
+        return str + "ay";
+    }
+
+    private String reshuffle(String str){
+        switch (str.toLowerCase()) {
+            case "the":
+                return str.charAt(2) + str.substring(0, 2);
+            case "over":
+                return str + "w";
+            case "brown":
+                return str.substring(2, str.length()) + str.substring(0, 2);
+            default:
+                return str.substring(1, str.length()) + str.charAt(0);
+        }
+    }
+
 }
