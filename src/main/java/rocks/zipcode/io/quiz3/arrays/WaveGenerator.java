7b6f7e41f74520d61777b37077252d02a9bed71e
MannyMonocle <eetumadu@gmail.com>
2019-11-18 14:46:38 -0500

    wave halfway

--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -4,7 +4,24 @@ package rocks.zipcode.io.quiz3.arrays;
  * @author leon on 09/12/2018.
  */
 public class WaveGenerator {
+
+    //LOGIC MAKES MORE SENSE BUT FAILS MORE TESTS
     public static String[] wave(String str) {
-        return null;
+        String[] ans = new String[str.length()];
+        if (str.length() == 0) {
+            return ans;
+        }
+
+        ans[0] = Character.toUpperCase(str.charAt(0)) + str.substring(1, str.length()).toLowerCase();
+
+        for (int i = 1; i < str.length() - 1; i++) {
+            ans[i] = str.substring(0, i).toLowerCase() + Character.toUpperCase(str.charAt(i)) + str.substring(i + 1, str.length()).toLowerCase();
+        }
+
+        ans[str.length() - 1] = str.substring(0, str.length() - 1).toLowerCase() + Character.toUpperCase(str.charAt(str.length() - 1));
+
+
+        return ans;
     }
+
 }
